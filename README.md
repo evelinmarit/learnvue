c65c46a0aac18a410d1c295eab30e103858d18d0
Evelin Maripuu <evelin.marit@gmail.com>
2025-02-28 21:15:12 +0200

    Pure js counter example

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+Programmiks peab olema installitud bun (bun.sh). Terminalis käsk: powershell -c "irm bun.sh/install.ps1 | iex"
+Tee terminalis projekti kaust (mkdir projekti nimi), 
+mine loodud kausta (terminalis: cd projekti nimi) ja 
+jooksuta käsk: $ bun create rsbuild@latest (vt rsbpack kodulehte https://rspack.dev/guide/start/quick-start)
+Tulevad valikud, kus valida vue 3, JavaScript (grupitöö puhul on teine valik ehk TextScript parem variant), Add Biome for code linting and formatting (valikutel liigu nooleklahvidega ja enne enterit vajuta space)
+Seejärel öeldakse terminalis ette mida edasi tegema peab.
+Peale projekti loomist VSCode avamiseks käsk: code .
+index.css faili on kopeeritud ligipääs Bulmale (lehelt: https://bulma.io/documentation/start/installation/)
+
+Käivita terminalis käsuga: bun run dev
+
 # Rsbuild project
 
 ## Setup
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => { 
+const button = document.QuerySelector('#btn');
+let count = document.QuerySelector('#count');
+let i = 0;
+count.innerHTML = i;
+console.log(button);
+button.addEventListener('click', () => {
+i++;
+count.innerHTML = i;
+});
+});
+</script>
+
 <template>
-  <div class="content">
-    <h1>Rsbuild with Vue</h1>
-    <p>Start building amazing things with Rsbuild.</p>
+  <div class="container content section">
+    <button id="btn" class="button is-primary">Click me</button>
+    <h1 id="count"></h1>
   </div>
+
 </template>
 
 <style scoped>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
 
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
-
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
 </style>
