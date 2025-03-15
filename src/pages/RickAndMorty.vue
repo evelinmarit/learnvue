f3d0c03d4e676d15d3cc11122764b4a1ad330568
Evelin Maripuu <evelin.marit@gmail.com>
2025-03-15 18:15:27 +0200

    Infinitive scroll

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from "axios";
-import { nextTick, ref } from "vue";
+import { nextTick, onMounted, ref } from "vue";
 import CharactersCard from "../components/CharactersCard.vue";
 import SimplePagination from "../components/SimplePagination.vue";
 import PagedPagination from "../components/PagedPagination.vue";
@@ -8,7 +8,7 @@ import PagedPagination from "../components/PagedPagination.vue";
 const pagination = ref({});
 const characters = ref({});
 const currentPage = ref(1);
-const SearchValue = ref('');
+const searchValue = ref('');
 let searchTimeout = null;
 
 getCharacters();
@@ -16,11 +16,11 @@ async function getCharacters() {
   let response = await axios.get("https://rickandmortyapi.com/api/character", {
     params: {
       page: currentPage.value,
-      name: SearchValue.value
+      name: searchValue.value
     }
   });
   pagination.value = response.data.info;
-  characters.value = response.data.results;
+  characters.value.push(...response.data.results);
 }
 
 async function next() {
@@ -42,15 +42,24 @@ async function search() {
   clearTimeout(searchTimeout);  // debounce
   searchTimeout = setTimeout(() => {
     currentPage.value = 1;
+    characters.value = [];
     getCharacters();    
   }, 600);
 }
+
+onMounted(() => {
+  document.addEventListener('scroll', () => {
+    if(window.scrollY + window.innerHeight > document.body.clientHeight - 300 && pagination.value.next){
+      next();
+    }
+  })
+})
 
 </script>
 <template>
   <div class="field has-addons">
   <div class="control is-expanded">
-    <input @input="search" class="input" type="text" placeholder="Search" v-model="SearchValue">
+    <input @input="search" class="input" type="text" placeholder="Search" v-model="searchValue">
   </div>
   <div class="control">
     <button @click="search" class="button is-primary">
@@ -58,10 +67,13 @@ async function search() {
     </button>
   </div>
 </div>
-  <PagedPagination :current = "currentPage" :pagination="pagination" @next="next" @prev="prev" @page="page"></PagedPagination>
+  <!-- <PagedPagination :current = "currentPage" :pagination="pagination" @next="next" @prev="prev" @page="page"></PagedPagination> -->
   <div class="columns is-multiline">
     <div class="column is-one-quarter" v-for="character in characters">
       <CharactersCard :character="character"></CharactersCard>
     </div>
   </div>
+  <section v-if="!pagination.next" class="section is-large content has-text-centered">
+    <h1>No more characters</h1>
+  </section>
 </template>
